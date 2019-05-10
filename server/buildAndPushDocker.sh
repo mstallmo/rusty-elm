223de1f7294d367725ff66cc4242364714c42673
Mason Stallmo <masonstallmo@gmail.com>
2019-05-09 20:23:16 -0500

    remove database_url parameter from build arg

--- server/buildAndPushDocker.sh
+++ server/buildAndPushDocker.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 docker login --username=_ --password=${HEROKU_AUTH_TOKEN} registry.heroku.com
 
-docker build . -t registry.heroku.com/rusty-elm/web --build-arg DATABASE_URL=${DATABASE_URL} --no-cache
+docker build . -t registry.heroku.com/rusty-elm/web --no-cache
 
 docker push registry.heroku.com/rusty-elm/web
