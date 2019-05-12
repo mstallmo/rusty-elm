e9314960c6f4354a7088281aa5652b382670c3f5
Mason Stallmo <masonstallmo@gmail.com>
2019-05-11 20:19:14 -0500

    make the const flag obvious it's a bool

--- webpack.config.js
+++ webpack.config.js
@@ -5,11 +5,11 @@ const dist = path.resolve(__dirname, "dist");
 const webpack = require("webpack");
 require("dotenv").config();
 
-const PRODUCTION = process.env.NODE_ENV === 'production';
+const IS_PRODUCTION = process.env.NODE_ENV === 'production';
 
 module.exports = {
   entry: "./js/bootstrap.js",
-  mode: (PRODUCTION) ? 'production' : 'development',
+  mode: (IS_PRODUCTION) ? 'production' : 'development',
   output: {
     path: dist,
     filename: "bundle.js"
@@ -30,7 +30,7 @@ module.exports = {
     new WasmPackPlugin({
       crateDirectory: path.resolve(__dirname, "crate"),
       // WasmPackPlugin defaults to compiling in "dev" profile. To change that, use forceMode: 'release':
-      forceMode: (PRODUCTION) ? 'release' : ''
+      forceMode: (IS_PRODUCTION) ? 'release' : ''
     }),
 
     new webpack.EnvironmentPlugin(["API_URL"])
@@ -43,7 +43,7 @@ module.exports = {
         loader: 'elm-webpack-loader',
         options: {
           debug: false,
-          optimize: PRODUCTION
+          optimize: IS_PRODUCTION
         }
       }
     ]
